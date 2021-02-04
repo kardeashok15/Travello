15a37e34d49012b7c665cdedd605e78e988980c0
kardeashok15 <75639588+kardeashok15@users.noreply.github.com>
2021-02-04 20:21:58 +0530

    Delete abk directory

--- abk/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from abk.models import Contact
-from abk.models import Destination
-from abk.models import memories
-# Register your models here.
-admin.site.register(Contact)
-admin.site.register(Destination)
-admin.site.register(memories)
